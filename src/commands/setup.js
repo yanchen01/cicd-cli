65475978a41c8e8db0a4b6d93687165b0849d0cd
Kaito Yamagishi <kaitoyamagishi1109@gmail.com>
2020-12-01 22:13:46 -0500

    testing removing symbols

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -171,7 +171,7 @@ class SetupCommand extends Command {
 						{
 							name: 'Executing OC Commands',
 							run:
-								'oc login ${{ secrets.OC_SERVER_URL }} --token=${{ secrets.OC_API_TOKEN }}\necho ${{ secrets.OC_SERVER_URL }}\noc start-build ${{ github.event.repository.name }} --follow || oc new-app https://www.github.com/${{ github.repository }} --name=${{ github.event.repository.name }}\n'
+								'#!/bin/bash\nrepoName=${{ github.event.repository.name }} | sed 's/[^a-zA-Z0-9]//g'\noc login --token=${{ secrets.OC_API_TOKEN }} --server=${{ secrets.OC_SERVER_URL }}\noc start-build --from-build=$repoName --follow || oc new-app https://www.github.com/${{ github.repository }} --name=$repoName'
 						}
 					]
 				}
