5c3323572de576fae0248926349562218902301d
Kaito Yamagishi <kaitoyamagishi1109@gmail.com>
2020-12-03 21:02:20 -0500

    deleting IT pod solidified, rolls out update if branch name pod already exists

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -117,7 +117,7 @@ class SetupCommand extends Command {
 						},
 						{
 							name: 'Executing OC Commands',
-							run: "#!/bin/bash\nbranchName=`(echo ${{ github.head_ref }} | tr '[:upper:]' '[:lower:]') | sed -e 's/[^a-z0-9]//g'`\noc login --token=${{ secrets.OC_API_TOKEN }} --server=${{ secrets.OC_SERVER_URL }}\noc new-app https://www.github.com/${{ github.repository }}#${{ github.head_ref }} --name=$branchName\n"
+							run: "#!/bin/bash\nbranchName=`(echo ${{ github.head_ref }} | tr '[:upper:]' '[:lower:]') | sed -e 's/[^a-z0-9]//g'`\noc login --token=${{ secrets.OC_API_TOKEN }} --server=${{ secrets.OC_SERVER_URL }}\noc start-build $branchName --follow || oc new-app https://www.github.com/${{ github.repository }}#${{ github.head_ref }} --name=$branchName\n"
 						}
 					]
 				},
@@ -138,7 +138,7 @@ class SetupCommand extends Command {
 						},
 						{
 							name: 'Executing OC Commands',
-							run: 'oc login --token=${{ secrets.OC_API_TOKEN }} --server=${{ secrets.OC_SERVER_URL }}\noc delete all --selector app=$branchName\n'
+							run: "#!/bin/bash\nbranchName=`(echo ${{ github.head_ref }} | tr '[:upper:]' '[:lower:]') | sed -e 's/[^a-z0-9]//g'`'oc login --token=${{ secrets.OC_API_TOKEN }} --server=${{ secrets.OC_SERVER_URL }}\noc delete all --selector app=$branchName\n"
 						}
 					]
 				}
